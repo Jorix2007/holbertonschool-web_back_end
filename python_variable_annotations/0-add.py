29fc3c99360adfa1a14f9c12feecbc58d2f846bf
Jorix2007 <jabirbusines@gmail.com>
2026-05-31 15:56:01 +0400

    local

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module add a float a and a float b as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns their sum as a float"""
+    return a + b
