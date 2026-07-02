5808b8df94ff4ad4f57a43adc385a22fadf556cc
Jorix2007 <jabirbusines@gmail.com>
2026-07-02 17:04:17 +0400

    lolcal

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Utility to calculate slice bounds for pagination."""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Computes the starting and ending indices
+    for a given page number and items per page.
+    """
+    offset = (page - 1) * page_size
+    limit = offset + page_size
+
+    return offset, limit
